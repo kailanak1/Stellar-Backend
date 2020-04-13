13f1cff19bad0ced226bd2487a6ed9f91784f5e2
clheard54 <clarion.heard@gmail.com>
2020-04-13 14:11:48 -0700

    constellations seeded and accessible

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   namespace 'api' do
     namespace 'v1' do
-      resources :user
-      resources :event
-      resources :calendar
+      resources :users
+      resources :events
+      resources :calendars
+      resources :constellations
       #double check these:
       post '/login', to: 'auth#create'
       get '/profile', to: 'users#profile'
